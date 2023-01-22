bc72cc58298d0825d13396b5e794a4f3fa84ce01
GXBWilsonZz <2529137863@qq.com>
2023-01-22 18:22:21 +0800

    Update BlogPostListPage.js

--- themes/next/components/BlogPostListPage.js
+++ themes/next/components/BlogPostListPage.js
@@ -18,7 +18,7 @@ const BlogPostListPage = ({ page = 1, posts = [], postCount }) => {
     return <BlogPostListEmpty />
   } else {
     return (
-      <div className='absolute top-2'>
+      <div className='relative top-2'>
         {/* 文章列表 */}
         <div id="container" className="flex flex-wrap w-3/4 mx-4 lg:space-y-4 space-y-1">
           {posts.map(post => (
